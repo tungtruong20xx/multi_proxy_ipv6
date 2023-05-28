ac62da09f7bd8865e047b930fb5390032959c43d
tungtruong20xx <70914233+tungtruong20xx@users.noreply.github.com>
2023-05-29 03:41:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$IP4/$port/$(gen64 $IP6)"
+        echo "//$IP4/$port/$(gen64 $IP6)"
     done
 }
 
