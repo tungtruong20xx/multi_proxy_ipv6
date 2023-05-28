63aab6177aa695bfe57ffd18c7956bc28905d24e
tungtruong20xx <70914233+tungtruong20xx@users.noreply.github.com>
2023-05-29 02:58:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    URL="https://raw.githubusercontent.com/tungtruong20xx/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
